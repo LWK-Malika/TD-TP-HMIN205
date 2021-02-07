c5e9a408c9833aaec5e6342d77fed887375ad2fc
LWK Malika <malika.lin-wee-kuan@etu.umontpellier.fr>
2021-02-07 23:58:29 +0400

    ajouts des captures + exercices du TP1

---
+++ Exo8/app/src/main/java/com/example/exo8/Train.java
@@ -0,0 +1,17 @@
+package com.example.exo8;
+
+public class Train {
+    public String Depart;
+    public String Arrivee;
+    public int Num;
+    public String Heure;
+
+    Train(String d,String a, int n, String h){
+        Depart = d;
+        Arrivee = a;
+        Num = n;
+        Heure = h;
+
+    }
+
+}
